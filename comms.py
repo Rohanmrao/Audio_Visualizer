2886862e6cbf7e0fe4f3296a7fe061e679053bbe
Rohan <rohanmaheshrao10@gmail.com>
2023-04-24 14:40:08 +0530

    sent prediction values

--- comms.py
+++ comms.py
@@ -5,9 +5,10 @@ s = socket.socket()
 s.bind(('0.0.0.0', 8090))
 s.listen(0)
 
-while True:
-    client, addr = s.accept()
-    print(f"Connection from {addr[0]}:{addr[1]}")
-    a=["hi","gimbul"]
-    client.send(','.join(a).encode())  # Convert list of strings to a single string and encode to bytes
-    client.close()
+def run_comms(listval):
+
+    while True:
+        client, addr = s.accept()
+        print(f"Connection from {addr[0]}:{addr[1]}")
+        client.send(','.join(listval).encode())  # Convert list of strings to a single string and encode to bytes
+        client.close()
