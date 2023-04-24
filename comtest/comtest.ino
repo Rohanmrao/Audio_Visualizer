d374357087397bd036a9874a3b308551a97cafba
Rohan <rohanmaheshrao10@gmail.com>
2023-04-24 14:50:06 +0530

    comtest now uses int for tempo

--- comtest/comtest.ino
+++ comtest/comtest.ino
@@ -1,11 +1,11 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 
-const char* ssid = "mamba";
-const char* password = "happy100";
+const char* ssid = "Saar Anna";
+const char* password = "mosranna";
 
 const uint16_t port = 8090;
-const char* host = "192.168.43.62";
+const char* host = "192.168.223.57";
 
 WiFiClient client;
 
@@ -45,23 +45,34 @@ void loop()
       
       // Split received data by comma
       int numElements = 0;
-      String elements[10]; // Assuming maximum of 10 elements
+      String elements[2]; // Assuming 2 elements: string and float
+      String currentElement;
       for (int i = 0; i < receivedData.length(); i++) {
         if (receivedData.charAt(i) == ',') {
+          elements[numElements] = currentElement;
+          currentElement = "";
           numElements++;
         } else {
-          elements[numElements] += receivedData.charAt(i);
+          currentElement += receivedData.charAt(i);
         }
       }
-      numElements++;
+      // Process the last element after the loop
+      if (!currentElement.isEmpty()) {
+        elements[numElements] = currentElement;
+        numElements++;
+      }
       
-      // Print elements separately
-      Serial.println("Elements:");
-      for (int i = 0; i < numElements; i++) {
-        Serial.print("Item ");
-        Serial.print(i);
-        Serial.print(": ");
-        Serial.println(elements[i]);
+      // Check if the expected number of elements is received
+      if (numElements == 2) {
+        String strElement = elements[0];
+        float floatElement = elements[1].toFloat(); // Convert string to float
+        Serial.println("Elements:");
+        Serial.print("String: ");
+        Serial.println(strElement);
+        Serial.print("Float: ");
+        Serial.println(floatElement);
+      } else {
+        Serial.println("Received data does not have expected number of elements");
       }
     }
   } else {
